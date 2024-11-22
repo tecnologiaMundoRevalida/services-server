07e1eda142b60cb6ece6eb3fcd7761312e89e2a9
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2024-11-22 07:57:38 -0300

    alterando modelo para o o1-mini

--- app/Jobs/GenerateCommentsForQuestionsJob.php
+++ app/Jobs/GenerateCommentsForQuestionsJob.php
@@ -323,7 +323,7 @@ class GenerateCommentsForQuestionsJob implements ShouldQueue
             // Create a thread
             TestProcessingLog::create(['test_id' => $this->test_id,'number_question' => $key,'log' => 'Start Create chat to Generate Comment']);
             $threadResponse = $client->chat()->create([
-                'model' => 'o1-preview',
+                'model' => 'o1-mini',
                 // gpt-4o
                 'messages' => [
                     ['role' => 'user', 'content' => $comment_prompt]
