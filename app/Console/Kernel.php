c7eb21dae3928bf34dc19e05ee0f1b56ff5a4cb7
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2025-01-30 10:57:08 -0300

    finalizando criacao de cron para retirar reserva de questoes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule):void
     {
         $schedule->command('reservations:deactivate')
-                ->dailyAt('13:10');
+                ->dailyAt('03:00');
     }
 
     /**
