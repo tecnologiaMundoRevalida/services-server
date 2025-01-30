ec7722ee0b2135b9513e133fd74d00ccd00108dd
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2025-01-30 10:04:30 -0300

    testando execucao de cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule):void
     {
         $schedule->command('reservations:deactivate')
-                ->dailyAt('12:50');
+                ->dailyAt('13:10');
     }
 
     /**
