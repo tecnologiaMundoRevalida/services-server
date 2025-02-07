ad96388d6242c7532955d510736ed9a4a34b506e
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2025-02-07 17:14:52 -0300

    testando mb_convert direto na question

--- resources/views/pdfs/pdf-test.blade.php
+++ resources/views/pdfs/pdf-test.blade.php
@@ -125,7 +125,7 @@
             </div>
 
             <div class="question-description">
-                {!! mb_convert_encoding(strip_tags($question->question, '<p><img><title>'), 'UTF-8', 'auto') !!}
+                {!! strip_tags($question->question, '<p><img><title>') !!}
             </div>
             <br>
 
