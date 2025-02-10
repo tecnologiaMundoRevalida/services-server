8a812164d6bfb2d2d7bed9aa49e14b583cae37a5
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2025-02-10 09:51:17 -0300

    setando fonte padrao

--- resources/views/pdfs/pdf-test.blade.php
+++ resources/views/pdfs/pdf-test.blade.php
@@ -125,7 +125,7 @@
             </div>
 
             <div class="question-description">
-                {!! iconv("ISO-8859-1", "UTF-8", strip_tags($question->question, '<p><img><title>')) !!}
+                {!! iconv("Windows-1252", "UTF-8", strip_tags($question->question, '<p><img><title>')) !!}
             </div>
             <br>
 
