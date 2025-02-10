54ef98d6ab0ed9b442aef2b60fd96e47109913bb
Elder Oliveira Ext <e.docarmooliveira@hotmail.com>
2025-02-10 10:54:31 -0300

    setando fonte padrao

--- app/Services/PDFTestService.php
+++ app/Services/PDFTestService.php
@@ -236,42 +236,20 @@ class PDFTestService
     }
 
     public function sanitize($texto){
-        $search = [                 // www.fileformat.info/info/unicode/<NUM>/ <NUM> = 2018
-                    "\xC2\xAB",     // « (U+00AB) in UTF-8
-                    "\xC2\xBB",     // » (U+00BB) in UTF-8
-                    "\xE2\x80\x98", // ‘ (U+2018) in UTF-8
-                    "\xE2\x80\x99", // ’ (U+2019) in UTF-8
-                    "\xE2\x80\x9A", // ‚ (U+201A) in UTF-8
-                    "\xE2\x80\x9B", // ‛ (U+201B) in UTF-8
-                    "\xE2\x80\x9C", // “ (U+201C) in UTF-8
-                    "\xE2\x80\x9D", // ” (U+201D) in UTF-8
-                    "\xE2\x80\x9E", // „ (U+201E) in UTF-8
-                    "\xE2\x80\x9F", // ‟ (U+201F) in UTF-8
-                    "\xE2\x80\xB9", // ‹ (U+2039) in UTF-8
-                    "\xE2\x80\xBA", // › (U+203A) in UTF-8
-                    "\xE2\x80\x93", // – (U+2013) in UTF-8
-                    "\xE2\x80\x94", // — (U+2014) in UTF-8
-                    "\xE2\x80\xA6"  // … (U+2026) in UTF-8
+        $search = [
+            "’", "‘", "“", "”",    // Aspas e apóstrofos “curvos”
+            "–", "—",               // Hífens curtos e longos
+            "…",                    // Reticências especiais
+            " ",                    // Espaço especial (non-breaking space 0xC2A0) 
+        ];
+    
+        $replace = [
+            "'", "'", "\"", "\"",   // substitui por aspas/linha reta
+            "-", "-",               // substitui hifens por hifen simples
+            "...",                  // substitui reticências
+            " ",                    // substitui espaço "especial" por espaço normal
         ];
 
-        $replacements = [
-                    "<<", 
-                    ">>",
-                    "'",
-                    "'",
-                    "'",
-                    "'",
-                    '"',
-                    '"',
-                    '"',
-                    '"',
-                    "<",
-                    ">",
-                    "-",
-                    "-",
-                    "..."
-        ];
-
-       return str_replace($search, $replacements, $texto);
+       return str_replace($search, $replace, $texto);
     }
 }
